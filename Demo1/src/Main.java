01bf6c2cf4850016f80595ed3e303e4cc77b8fb8
ntkimlinh <86461274+ntkimlinh@users.noreply.github.com>
2026-06-18 19:36:29 +0700

    Push code java co ban

---
+++ Demo1/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello Testing 16!");
+
+        int number1=10;
+        String string1 = "hello Kim Linh";
+
+        System.out.println(number1);
+        System.out.println(string1);
+
+    }
+}
